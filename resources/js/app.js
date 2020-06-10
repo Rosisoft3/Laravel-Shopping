5ce42f21badb4a69a02b64b9552f731ced9a8dd2
Rosisoft3 <xxrdxx3@gmail.com>
2020-06-10 12:28:06 +0200

    20200610-1130 Authentication

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+require('./materialize.js');
+
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+  
+  var elems = document.querySelectorAll('.tooltipped');
+  M.Tooltip.init(elems);
+});
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::post('deconnexion', 'Auth\LoginController@logout')->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::prefix('connexion')->group(function () {
+        Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+        Route::post('/', 'Auth\LoginController@login');
+    });
+    Route::prefix('inscription')->group(function () {
+        Route::get('/', 'Auth\RegisterController@showRegistrationForm')->name('register');
+        Route::post('/', 'Auth\RegisterController@register');
+    });
+});
+Route::prefix('passe')->group(function () {
+    Route::get('renouvellement', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('renouvellement/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('renouvellement', 'Auth\ResetPasswordController@reset')->name('password.update');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
