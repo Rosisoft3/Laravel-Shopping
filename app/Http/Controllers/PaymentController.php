cb004e8c20a87a76b2382c3ef2d2fd19dec6ece3
Rosisoft3 <xxrdxx3@gmail.com>
2020-06-14 12:10:30 +0200

    20200614-1110 Invocies Problem

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\Facture;
 use App\Models\{ Order, State };
 
 class PaymentController extends Controller
@@ -11,10 +12,12 @@ class PaymentController extends Controller
     /**
      * Manage payment
      *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Services\Facture  $facture
      * @param  \App\Models\Order  $order
      * @return void
      */
-    public function __invoke(Request $request, Order $order)
+    public function __invoke(Request $request, Facture $facture, Order $order)
     {
         $this->authorize('manage', $order);
 
@@ -25,11 +28,17 @@ class PaymentController extends Controller
         } else {
             \Stripe\Stripe::setApiKey(config('stripe.secret_key'));
             $intent = \Stripe\PaymentIntent::retrieve($request->payment_intent_id);
-            if ($intent->status === 'succeeded') {
+            if ($intent->status === 'succeeded') {              
                 $request->session()->forget($order->reference); 
                 $order->payment_infos()->create(['payment_id' => $intent->id]);         
-                $state = 'paiement_ok';  
-                // Création de la facture à prévoir              
+                $state = 'paiement_ok';
+                // Création de la facture
+                $response = $facture->create($order, true);  
+                if($response->successful()) {
+                    $data = json_decode($response->body());
+                    $order->invoice_id = $data->id;
+                    $order->invoice_number = $data->number;
+                }           
             } else {
                 $state = 'erreur';     
             }
